ed194240606a4c698b0727e77a8fc00031316e8a
Bianca-Morris <bmorris@wellesley.edu>
2017-12-13 15:01:43 -0800

    reorganize db-connect and express-session instances so tables all sync together

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// import database connection for use with sessions
-const { sequelize } = require('./models');
+// import database connections & express-session instance
+const { session } = require('./models');
+const { sessionStore } = require('./models');
 
 // import models
 const { User } = require('./models');
@@ -17,13 +18,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// Make SequelizeStore create/sync a table in DB
-var sessionStore = new SequelizeStore({
-  db: sequelize
-});
 
 // Tell express-session to use postgres to store sessions
 app.use(session({
@@ -32,8 +26,6 @@ app.use(session({
   resave: false,
   proxy: true
 }));
-
-sessionStore.sync();
 
 // Define passport's local strategy; how will user data be retrieved?
 passport.use(new LocalStrategy(
