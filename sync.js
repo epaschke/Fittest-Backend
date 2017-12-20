26ba382cff34bd962072e1b8855c8616b1450f41
Bianca-Morris <bmorris@wellesley.edu>
2017-12-20 11:09:02 -0800

    Update sync.js to es6, fix activities

--- sync.js
+++ sync.js
@@ -7,23 +7,23 @@ const {createGroup} = require('./dummyData');
 const {addMember} = require('./dummyData');
 const {addActivities} = require('./dummyData');
 
-models.sequelize.sync({ force: true })
-  .then(() => {
-      console.log('Successfully updated database tables!');
-      sessionStore.sync(); // Ensure Sessions table is updated
-      console.log('Successfully created sessions table!');
-      return;
-  })
-  .then(() => {
-     generateUsers();
-     createGroup(2, "Jogging Buddiezzz", "Friends who run together.", Date.now());
-     addMember(1, 3); // adds user #3 to group 1
-     createGroup(1, "Tennis Friends", "Friends who play tennis.", Date.now());
-     addMember(2, 4); // adds user #4 to group 2
-     console.log("Completed successfully. Exiting...")
-     addActivities();
-  })
-  .catch((error) => {
-      console.log('Error updating database tables', error);
-  	  process.exit(1);
-  });
+const syncData = async () => {
+  try {
+    await models.sequelize.sync({ force: true })
+    console.log('Successfully updated database tables!');
+    await sessionStore.sync(); // Ensure Sessions table is updated
+    console.log('Successfully created sessions table!');
+     await generateUsers();
+     await createGroup(2, "Jogging Buddiezzz", "Friends who run together.", new Date);
+     await addMember(1, 3); // adds user #3 to group 1
+     await createGroup(1, "Tennis Friends", "Friends who play tennis.", new Date);
+     await addMember(2, 4); // adds user #4 to group 2
+     await addActivities();
+   }
+catch(e){
+  console.log('Error updating database tables', e);
+  process.exit(1);
+  }
+};
+
+syncData();
