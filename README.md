c784e6adc3f81f8cf69067df8ad5dc3fdeef5960
Emma Paschke <epaschke@outlook.com>
2018-03-16 11:20:02 -0700

    add comments & update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Fittest-Backend
 
 App that allows users to track exercise and compete with Facebook friends in weekly tournaments. Backend was built with SQL, using Sequelize for ease of language. Backend and SQL database were deployed to Heroku at https://fit-fun.herokuapp.com.
+
+Model definitions can be found at [models.js](/models.js) and routes can be found at [routes.js](/routes.js).
--- routes.js
+++ routes.js
@@ -1,12 +1,8 @@
 const express = require('express');
-// const router = express();
 const router = express.Router();
-// const bodyParser = require('body-parser');
-// const PORT = process.env.PORT || 3000;
 const { User, Group, Activity, Membership, Trophy, Tourney, sequelize } = require('./models');
 const Op = sequelize.Op;
 const axios = require('axios');
-// router.use(bodyParser.json());
 const FB = require('fb');
 
 const calcEndFn = (start) => {
@@ -16,8 +12,10 @@ const calcEndFn = (start) => {
 };
 
 //CREATION ROUTES
+//create a new group
 router.post('/new/group', async (req, res) => {
   try {
+    //create group
     const groupId = await Group.create({
       name: req.body.name,
       description: req.body.description,
@@ -25,12 +23,14 @@ router.post('/new/group', async (req, res) => {
       groupImg: req.body.groupImg,
       startDate: new Date(req.body.startDate).toJSON()
     });
+    //add self to group as admin
     await Membership.create({
       active: true,
       role: 'admin',
       groupId: groupId.dataValues.id,
       userId: req.user.id
     });
+    //add tournament data
     await Tourney.create({
       groupId: groupId.dataValues.id,
       startDate: new Date(req.body.startDate).toJSON(),
@@ -44,6 +44,7 @@ router.post('/new/group', async (req, res) => {
   };
 });
 
+//add new activity to self's profile
 router.post('/new/activity', async (req, res) => {
   try {
     if (!req.body.name || !req.body.duration || !req.body.rigor) {
@@ -60,6 +61,7 @@ router.post('/new/activity', async (req, res) => {
       default:
         points = req.body.duration * 30;
     };
+    //create new activity for self
     const newActivity = await Activity.create({
       name: req.body.name,
       duration: req.body.duration,
@@ -75,13 +77,16 @@ router.post('/new/activity', async (req, res) => {
   };
 });
 
+//add self or other person to group
 router.post('/new/membership/:groupid', async (req, res) => {
   try {
     if (!parseInt(req.params.groupid)) {
       throw ("Group id must be an integer.");
     };
     if (req.body.addId){
-      // an invitation -- with active: false
+      // if "addId" is a field, self is inviting someone else
+      // makes new relationship with active set to false
+      // invited user accepts by setting active to true
       await Membership.create({
         active: false,
         role: 'member',
@@ -90,6 +95,8 @@ router.post('/new/membership/:groupid', async (req, res) => {
       });
       res.status(200).json({"success": true});
     } else {
+      //otherwise, self is adding self to group
+      //active is true
       await Membership.create({
         active: true,
         role: 'member',
@@ -105,72 +112,7 @@ router.post('/new/membership/:groupid', async (req, res) => {
   };
 });
 
-//TEMPORARY FORCE END TOURNEY
-router.get('/end/tourney/:tourneyid', async (req, res) => {
-  try {
-    if (!parseInt(req.params.tourneyid)) {
-      throw ("Tourney id must be an integer.");
-    };
-    const tourneySE = await Tourney.findOne({
-      where: { id: req.params.tourneyid },
-      attributes: [ "startDate", "endDate" ]
-    });
-    const tourney = await Tourney.findOne({
-      where: { id: req.params.tourneyid },
-      include: {
-        model: Group,
-        attributes: [ "name" ],
-        include: {
-          model: User,
-          attributes: [ "id" ],
-          where: { public: true },
-          through: { model: Membership, attributes: [] },
-          include: {
-            model: Activity,
-            attributes: ['points'],
-            where: { createdAt: { [Op.between]: [tourneySE.dataValues.startDate, tourneySE.dataValues.endDate] } }
-          }
-        }
-      }
-    });
-    let hashTotals = {};
-    let sort = tourney.group.users.map(u => {
-      let total = 0;
-      switch(u.dataValues.activities.length){
-        case 0:
-          break;
-        case 1:
-          total = u.dataValues.activities[0].points;
-          break;
-        default:
-          total = u.dataValues.activities.reduce((a, b) => {
-            return a.dataValues.points + b.dataValues.points;
-          });
-      }
-      hashTotals[u.id] = total;
-      return total;
-    });
-    let winning = sort.sort()[0];
-    let winner;
-    Object.keys(hashTotals).map(userid => {
-      if (hashTotals[userid] === winning){
-        winner = userid
-      }
-    });
-    await Trophy.create({
-      date: tourney.endDate,
-      points: hashTotals[winner],
-      userId: winner,
-      tourneyId: tourney.id
-    });
-    res.status(200).json({"success": true, tourney, winner })
-  }
-  catch (e) {
-    console.log("Error while ending tourney: ", e);
-    res.status(500).json({"success": false, "error": e})
-  }
-});
-
+//view self profile
 router.get('/view/user', async (req, res) => {
   try {
     const user = await User.findOne({ where: { id: req.user.id } });
@@ -183,6 +125,7 @@ router.get('/view/user', async (req, res) => {
 })
 
 //EDIT ROUTES
+//change another user to be an admin, current user must be an admin to make this change
 router.post('/edit/membership/:groupid', async (req, res) => {
   try {
     if (!parseInt(req.params.groupid)) {
@@ -206,11 +149,13 @@ router.post('/edit/membership/:groupid', async (req, res) => {
   };
 });
 
+//current user accepts invitation
 router.get('/accept/invite/:groupid', async (req, res) => {
   try {
     if (!parseInt(req.params.groupid)) {
       throw ("Group id must be an integer.");
     };
+    //change membership to active to accept
     await Membership.update({ active: true },
     { where: { groupId: req.params.groupid, userId: req.user.id } });
     res.status(200).json({ "success": true });
@@ -222,20 +167,23 @@ router.get('/accept/invite/:groupid', async (req, res) => {
 })
 
 //SEARCH ROUTES
+//search for invited, public, or active groups
 router.get('/search/:term', async (req, res) => {
   try {
     if (parseInt(req.params.term)) {
       throw ("Search term should be a string; either: 'invited', 'public', or 'active'. No groups found.");
     };
     switch(req.params.term){
+      //groups self has been invited to
       case 'invited':
         const invitedGroups = await Group.findAll({
-          include: [{
+          include: [{ //get members of group
             model: User,
             attributes: [ 'id', 'username', 'img' ],
             through: Membership
           }]
         });
+        //filter groups by those with active === false (invited) && user has relationship
         let invited = invitedGroups.filter((group) => {
           if (group.users.filter((user) => {
             return user.membership.active === false && user.membership.userId === req.user.id;
@@ -245,9 +193,10 @@ router.get('/search/:term', async (req, res) => {
         });
         res.status(200).json({ "success": true, "groups": invited });
         break;
+      //groups that are public to join
       case 'public':
         const publicGroups = await Group.findAll({
-          include: {
+          include: { // get members of group
             model: User,
             attributes: ['id', 'username'],
             through: { model: Membership, where: { active: true } }},
@@ -255,13 +204,15 @@ router.get('/search/:term', async (req, res) => {
         });
         res.status(200).json({ "success": true, "groups": publicGroups});
         break;
+      //groups that the user is currently in
       case 'active':
         const myGroups = await Group.findAll({
-          include: [{
+          include: [{ // get members
             model: User, attributes: ['id', 'username', 'img'],
             through: Membership
           }]
         });
+        //filter by user's membership
         let filteredGroups = myGroups.filter((group) => {
           if (group.users.filter((user) => {
             return user.membership.active === true && user.membership.userId === req.user.id;
@@ -281,6 +232,7 @@ router.get('/search/:term', async (req, res) => {
   };
 });
 
+//get information for a single group
 router.get('/groups/:groupid', async (req, res) => {
   try {
     if (!parseInt(req.params.groupid)) {
@@ -288,12 +240,12 @@ router.get('/groups/:groupid', async (req, res) => {
     };
     let group = await Group.findOne({
       where: { id: parseInt(req.params.groupid) },
-      include: [{
+      include: [{ // include members
         model: User,
         through: { model: Membership, attributes: ["role"] },
         include: { model: Activity, attributes: {exclude: ['id', 'userId', 'updatedAt']} }
       }, {
-        model: Tourney,
+        model: Tourney, // include current tournament
         include: Trophy
       }]
     });
@@ -301,17 +253,18 @@ router.get('/groups/:groupid', async (req, res) => {
     res.status(200).json({"success": true, group });
   }
   catch (e) {
-    console.log('Error getting active groups:', e);
+    console.log('Error getting single group:', e);
     res.status(500).json({ "success": false, "error": e });
   };
 });
 
+//remove self from group
 router.get('/leave/group/:groupid', async (req, res) => {
   try {
     if (parseInt(req.params.groupid).isNan()){
       throw "Group Id is not a number"
     }
-    await Membership.destroy({
+    await Membership.destroy({ // delete relationship
       where: { userId: req.user.id, groupId: parseInt(req.params.groupid)}
     });
     res.status(200).json({"success": true})
@@ -323,10 +276,11 @@ router.get('/leave/group/:groupid', async (req, res) => {
 })
 
 //USER ROUTES
+//get self history of exercise
 router.get('/user/history', async (req, res) => {
   try {
-    const history = await Activity.findAll({ where: { userId: req.user.id } });
-    const totalPoints = await Activity.findAll({
+    const history = await Activity.findAll({ where: { userId: req.user.id } }); //all activities
+    const totalPoints = await Activity.findAll({ // calculate points
       where: { userId: req.user.id },
       attributes: [[sequelize.fn('sum', sequelize.col('points')), 'total']]
     });
@@ -338,6 +292,7 @@ router.get('/user/history', async (req, res) => {
   };
 });
 
+//change self between public and private
 router.get('/user/toggle/public', async (req, res) => {
   try {
     const current = await User.findOne({ where: { id: req.user.id }, attributes: ['public'] });
@@ -350,12 +305,13 @@ router.get('/user/toggle/public', async (req, res) => {
   }
 });
 
+//access user's trophy history
 router.get('/user/trophies', async (req, res) => {
   try {
     const trophies = await Trophy.findAll({
-      where: { userId: req.user.id },
+      where: { userId: req.user.id }, // where user owns Trophy
       attributes: { exclude: ["createdAt", "updatedAt", "tourneyId"] },
-      include: {
+      include: { // include tournament associated with Trophy
         model: Tourney,
         attributes: { exclude: ["createdAt", "updatedAt", "groupId"] },
         include: { model: Group, attributes: ["name", "id"] }
@@ -369,13 +325,14 @@ router.get('/user/trophies', async (req, res) => {
   }
 });
 
+//get winners for a single group
 router.get('/winners/:groupid', async (req, res) => {
   try {
     if (parseInt(req.params.groupid).isNan()){
       throw "Group Id is not a number"
     }
     const winners = await Trophy.findAll({
-      include: [
+      include: [ // include Tournament and User associated with Trophy
         { model: Tourney, where: { groupId: parseInt(req.params.groupid)}},
         { model: User, attributes: ['id', 'username', 'img']}
       ]
@@ -389,19 +346,21 @@ router.get('/winners/:groupid', async (req, res) => {
   }
 })
 
+//get another user's information
 router.get('/user/:userid', async(req, res) => {
   try {
     if (!parseInt(req.params.userid)) {
       throw ("User id must be an integer.");
     };
+    //find user
     const foundUser = await User.findOne({
       where: { id: req.params.userid, public: true },
-      attributes: { exclude: ["fbId"] },
-      include: {
+      attributes: { exclude: ["fbId"] }, //hide certain things for privacy
+      include: { // include groups
         model: Group,
         through: {
           model: Membership,
-          where: { userId: {
+          where: { userId: { // common groups
             [Op.or]: [req.params.userid, req.user.id] } },
           attributes: []
         }
@@ -463,12 +422,5 @@ router.get('/my/friends', (req, res) => {
   })
 });
 
-// router.get('/all/users', (req, res) => {
-//
-// });
 
 module.exports = router;
-
-// router.listen(PORT, error => {
-// error ? console.error(error) : console.log(`==> Listening on port ${PORT}.`);
-// });
